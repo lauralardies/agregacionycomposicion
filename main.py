d760adc79b0fe8d622d684bb01369305ff1be029
lauralardies <lrodrlar@myuax.com>
2022-03-22 09:11:26 +0100

    añadimos el código principal al main

---
+++ main.py
@@ -0,0 +1,16 @@
+# Ejercicio 2
+print("\nEjercicio 2:\n")
+
+yin = Yin()
+yang = Yang()
+yin.yang = yang
+
+print(yang)
+print(yang == yin.yang)
+del(yin.yang)
+del(yang)
+print("?")
+
+# El problema que teníamos era que había dos instancias de la clase Yang. Sólo borrábamos una de ellas y por lo tanto la clase
+# Yang todavía existía en la otra instancia. Al terminar el programa se borraba esa segunda instancia y cuando ya no había más 
+# instancias de la clase Yang era cuando se mostraba el mensaje. El problema se arregla borrando las dos instancias de Yang.
--- yinyang.py
+++ yinyang.py
@@ -5,18 +5,3 @@ class Yang:
 
     def __del__ (self):
         print ("Yang destruido")
-
-
-yin = Yin()
-yang = Yang()
-yin.yang = yang
-
-print(yang)
-print(yang == yin.yang)
-del(yin.yang)
-del(yang)
-print("?")
-
-# El problema que teníamos era que había dos instancias de la clase Yang. Sólo borrábamos una de ellas y por lo tanto la clase
-# Yang todavía existía en la otra instancia. Al terminar el programa se borraba esa segunda instancia y cuando ya no había más 
-# instancias de la clase Yang era cuando se mostraba el mensaje. El problema se arregla borrando las dos instancias de Yang.
